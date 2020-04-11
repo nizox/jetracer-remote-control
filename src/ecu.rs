809364443064d69f5015bdeca48b3c5b7bfe0ace
Nicolas Vivet <nicolas@nzx.io>
2020-04-12 01:25:02 +0200

    Read command from stdin

--- src/ecu.rs
+++ src/ecu.rs
@@ -1,22 +1,36 @@
 use actix::prelude::*;
 
-pub struct ECU;
+pub struct ECU {
+    pub pwm: pca9685::Pca9685<hal::I2cdev>,
+}
+
+pub struct Command(pub String);
 
 impl Actor for ECU {
     type Context = Context<Self>;
 
     fn started(&mut self, _ctx: &mut Self::Context) {
-        let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
-        let address = pca9685::SlaveAddr::default();
-        let mut pwm = pca9685::Pca9685::new(dev, address);
         // This corresponds to a frequency of 60 Hz.
-        pwm.set_prescale(100).unwrap();
+        self.pwm.set_prescale(100).unwrap();
 
         // Turn on channel 0 at 0.
-        pwm.set_channel_on(pca9685::Channel::C0, 0).unwrap();
+        self.pwm.set_channel_on(pca9685::Channel::C0, 0).unwrap();
 
         // Turn off channel 1 at 2047, which is 50% in
         // the range `[0..4095]`.
-        pwm.set_channel_off(pca9685::Channel::C1, 2047).unwrap();
+        self.pwm.set_channel_off(pca9685::Channel::C1, 2047).unwrap();
     }
+}
+
+impl Message for Command {
+    type Result = ();
+}
+
+impl Handler<Command> for ECU {
+    type Result = ();
+
+    fn handle(&mut self, msg: Command, _ctx: &mut Context<Self>) -> Self::Result {
+        println!("Got {:?}", msg.0);
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,21 @@ mod ecu;
 
 fn main() {
     let system = System::new("test");
-    let addr = ecu::ECU.start();
-    system.run();
+
+    let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
+    let slave = pca9685::SlaveAddr::default();
+    let addr = ecu::ECU{pwm: pca9685::Pca9685::new(dev, slave)}.start();
+
+    Arbiter::spawn(async {
+        std::thread::spawn(move || loop {
+            let mut cmd = String::new();
+            if std::io::stdin().read_line(&mut cmd).is_err() {
+                println!("error");
+                return;
+            }
+            addr.do_send(ecu::Command(cmd));
+        });
+    });
+
+    system.run().unwrap();
 }
